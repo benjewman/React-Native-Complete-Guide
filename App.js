66778016a8b0f30f60e0a6a5e010d0ff742b03f0
benjewman <bennyadave@gmail.com>
2020-07-28 13:38:08 -0400

    Add TextInput and Button while deleting Text

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }
