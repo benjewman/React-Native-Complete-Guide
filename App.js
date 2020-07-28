121dc224bd6e23bed7dbd4ca096d4e506b7b5fff
benjewman <bennyadave@gmail.com>
2020-07-28 16:32:36 -0400

    Wrap goals in ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -25,9 +25,13 @@ export default function App() {
         />
         <Button onPress={addGoalHandler} title="ADD" />
       </View>
-      <View>
-        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
-      </View>
+      <ScrollView>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>)
+        )}
+      </ScrollView>
     </View>
   );
 }
