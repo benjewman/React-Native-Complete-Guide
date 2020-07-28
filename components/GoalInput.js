bbce2ccd7b0d7e746e82a431271170c2c86b63cb
benjewman <bennyadave@gmail.com>
2020-07-28 18:41:06 -0400

    Change button style

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -22,8 +22,14 @@ const GoalInput = props => {
                 placeholder="Course Goal"
                 style={styles.input}
                 />
-                <Button title='CANCEL' color='red' onPress={props.onCancel}/>
-                <Button onPress={addGoalHandler} title="ADD" />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title='CANCEL' color='red' onPress={props.onCancel}/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button onPress={addGoalHandler} title="ADD" />
+                    </View>
+                </View>
             </View>
         </Modal>
     )
@@ -41,6 +47,14 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         marginBottom: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '80%'
+    },
+    button: {
+        width: '40%'
     }
 })
 
