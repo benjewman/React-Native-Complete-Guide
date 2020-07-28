c2f5c8411b229744e002a70e0490c78b5d2aee47
benjewman <bennyadave@gmail.com>
2020-07-28 13:52:42 -0400

    Delete Button and TextInput

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ export default function App() {
   return (
     <View style={{padding: 50}}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, width: '80%'}}/>
-        <Button title="ADD" />
+        
+      </View>
+      <View>
+
       </View>
       <View>
 
